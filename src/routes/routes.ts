b55853fc6fe209fa912754b3b02d96b417f3c5dd
RodrigoAragon <rodrixgo18@gmail.com>
2025-02-13 17:56:04 -0300

    Elimamos codigo innecesario

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -13,10 +13,6 @@ interface Route{
 
 const LazyLayout = lazy(() => import(/**webPackChunkName: "LazyLayout" */ '../01-lazyload/layout/LazyLayout'))
 
-const Lazy1 = lazy(() => import(/**webPackChunkName: "LazyPage1" */ '../01-lazyload/pages/LazyPage1'))
-const Lazy2 = lazy(() => import(/**webPackChunkName: "LazyPage2" */'../01-lazyload/pages/LazyPage2'))
-const Lazy3 = lazy(() => import(/**webPackChunkName: "LazyPage3" */'../01-lazyload/pages/LazyPage3'))
-
 export const routes: Route[] = [
     {
         to: '/lazyload/',
