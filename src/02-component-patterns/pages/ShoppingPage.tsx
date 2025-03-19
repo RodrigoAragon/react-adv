477719ca033111d440026c738dd0f4c9ba3224bf
RodrigoAragon <rodrixgo18@gmail.com>
2025-03-19 09:52:25 -0300

    pre-npm listo

--- src/02-component-patterns/pages/ShoppingPage.tsx
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -1,13 +1,10 @@
 import { ProductCard, ProductButtons, ProductImage, ProductTitle } from "../components"
 import '../styles/custom-styles.css'
-import { useShoppingCart } from "../hooks/useShoppingCart";
 import { products } from "../data/products";
 
 const product = products[0]
 
 export const ShoppingPage = () => {
-
-    const {shoppingCart, onProductCountChange} = useShoppingCart()
 
   return (
     <div>
@@ -16,26 +13,17 @@ export const ShoppingPage = () => {
           <ProductCard 
             key={product.id}
             product={product}
-            className = "bg-dark"
             initialValues = {{
               count:4,
               maxCount:10
             }}
           >
             {
-              ({reset, isMaxCountReached, increase, count}) =>(
+              () =>(
                 <>
-                  <ProductImage className="custom-image" style={{ boxShadow:'10px 10px 10px rgba(0,0,0,0.2)' }}/>
-                  <ProductTitle className="text-white text-bold"/>
-                  <ProductButtons className="custom-buttons"/>
-                  <button onClick={reset}>Reset</button>
-
-                  <button onClick={() => increase(-2)}>-2</button>
-                  {/*Si no se llega al maxCount, mostrar, sino ocultar*/}
-                  <button onClick={() => increase(+2)} hidden={isMaxCountReached}>+2</button>
-
-                  <span>{count}</span>
-                  
+                  <ProductImage/>
+                  <ProductTitle/>
+                  <ProductButtons/>
                 </>
               )
             }
